abb485f882b5ace41701fca137cadd7b6dc71a06
NiklasWeinreich <niklasweinreichhansen@hotmail.com>
2025-05-15 20:44:36 +0200

    game update

--- Frontend/src/app/Services/Games/dice.service.ts
+++ Frontend/src/app/Services/Games/dice.service.ts
@@ -2,16 +2,32 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from '../../Environments/environment';
 import { DiceGame } from '../../Models/dicegame.model';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { AuthService } from '../Security/auth.service';
 
 @Injectable({ providedIn: 'root' })
 export class DiceService {
   private apiUrl = environment.apiUrl + '/Dice/playGame';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   playGame(playerNumber: number, isGuessOver: boolean, betAmount: number): Observable<DiceGame> {
-    const body = { playerNumber, isGuessOver, betAmount };
+    const currentUser = this.authService.currentUserValue;
+    const userId = currentUser?.id;
+    const gameId = 1;
+
+    if (!userId) {
+      return throwError(() => new Error('Bruger ikke logget ind.'));
+    }
+
+    const body = {
+      userId,
+      gameId,
+      playerNumber,
+      isGuessOver,
+      betAmount
+    };
+
     return this.http.post<DiceGame>(this.apiUrl, body);
   }
 }
